a0200bb954e4d3a0ea13f45f43e62b6f7704fd48
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-04 02:26:31 +0700

    feat:user can send message to Apiwat email via contact

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Textarea = ({title,name,id}) => {
+const Textarea = ({title,name,id,value,onChange}) => {
+  
   return (
     <div className='w-full flex flex-col justify-between items-center gap-3 lg:w-[680px] lg:gap-3'>
         <h2 className='font-medium'>{title}</h2>
-        <textarea name={name} id={id} cols="45" rows="5" className='border w-52 p-2 text-center outline-none lg:w-5/6 rounded-lg' />
+        <textarea name={name} id={id} cols="45" rows="5" className='border w-52 p-2 text-center outline-none lg:w-5/6 rounded-lg'
+        value={value}
+        onChange={onChange} />
     </div>
   )
 }
