a3215b19cb77f526cbd83c07addd53983a86e57d
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-06 22:46:30 +0700

    fix:adjust animation on mystories to be more smoothly

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -26,33 +26,63 @@ const Blog = () => {
       :
       item.id % 2 === 0 ?
               <motion.section 
-              initial={'offscreen'}
-              whileInView={'onscreen'}
-              transition={{duration:1}}
-              viewport={{once:true,amount:0.5}}
-              variants={topAnimate}
-              className='h-auto flex flex-col justify-center items-center gap-10 my-2' 
-              key={item.id}>
+                initial={'offscreen'}
+                whileInView={'onscreen'}
+                transition={{duration:0.5}}
+                viewport={{once:true,amount:0.5}}
+                variants={topAnimate}
+                className='h-auto flex flex-col justify-center items-center gap-10 my-2' 
+                key={item.id}>
+
                 <h1 className='text-lg font-bold text-gray-700'>{item.title}</h1>
+
                 <div className='flex flex-col-reverse gap-5 lg:flex-row lg:items-center lg:justify-center shadow-lg'>
-                  <p className='w-[300px] p-5 text-center text-gray-600 lg:text-md lg:w-[600px] lg:p-10'>{item.description}</p>
-                  <img src={item.img} alt="me" className='w-[300px] lg:w-[500px] shadow-2xl rounded-lg'/>
+                  <motion.p 
+                     initial={'offscreen'}
+                     whileInView={'onscreen'}
+                     transition={{duration:0.7,delay:0.4}}
+                     viewport={{once:true,amount:0.5}}
+                     variants={topAnimate}
+                    className='w-[300px] p-5 text-center text-gray-600 lg:text-md lg:w-[600px] lg:p-10'>{item.description}</motion.p>
+                  <motion.img 
+                    initial={'offscreen'}
+                    whileInView={'onscreen'}
+                    transition={{duration:0.7,delay:0.2}}
+                    viewport={{once:true,amount:0.5}}
+                    variants={topAnimate}
+                    src={item.img} alt="me" className='w-[300px] lg:w-[500px] shadow-2xl rounded-lg'/>
                 </div>
               </motion.section>
       :
               <motion.div 
               initial={'offscreen'}
               whileInView={'onscreen'}
-              transition={{duration:1}}
+              transition={{duration:0.5}}
               viewport={{once:true,amount:0.5}}
               variants={topAnimate}
               className='h-auto flex flex-col justify-center items-center gap-10 my-2' 
               key={item.id}>
+
                   <h1 className='text-lg font-bold text-gray-700'>{item.title}</h1>
+
                   <div className='flex flex-col gap-5 lg:flex-row lg:items-center lg:justify-center shadow-lg'>
-                    <img src={item.img} alt="me" className='w-[300px] lg:w-[500px] shadow-2xl rounded-lg'/>
-                    <p className='w-[300px] p-5 text-center text-gray-600 lg:text-md lg:w-[600px] lg:p-10'>{item.description}</p>
+                    <motion.img 
+                    initial={'offscreen'}
+                    whileInView={'onscreen'}
+                    transition={{duration:0.7,delay:0.2}}
+                    viewport={{once:true,amount:0.5}}
+                    variants={topAnimate}
+                    src={item.img} alt="me" className='w-[300px] lg:w-[500px] shadow-2xl rounded-lg'/>
+
+                    <motion.p 
+                    initial={'offscreen'}
+                    whileInView={'onscreen'}
+                    transition={{duration:0.7,delay:0.4}}
+                    viewport={{once:true,amount:0.5}}
+                    variants={topAnimate}
+                    className='w-[300px] p-5 text-center text-gray-600 lg:text-md lg:w-[600px] lg:p-10'>{item.description}</motion.p>
                   </div>
+
               </motion.div>
      
     )
@@ -69,7 +99,7 @@ const Blog = () => {
           <motion.p 
             initial={'offscreen'}
             whileInView={'onscreen'}
-            viewport={{once:true,amount:0.5}}
+            viewport={{once:true,amount:0.8}}
             transition={{duration:1}}
             variants={topAnimate}
             className='text-base text-center font-md w-72 text-gray-600 px-10 py-4 lg:text-xl lg:w-[1100px]'>
