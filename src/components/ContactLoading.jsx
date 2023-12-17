7fea939f70e8e234a2c05fea168bf08673972417
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-18 02:38:54 +0700

    feat:add _redirect

--- src/components/ContactLoading.jsx
+++ src/components/ContactLoading.jsx
@@ -21,34 +21,34 @@ const ContactLoading = ({isLoading}) => {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
-    <ModalOverlay />
-    <ModalContent
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      background="none"
-      boxShadow="none"
-    >
-      <div className="w-screen h-screen backdrop-blur-sm z-50 flex justify-center items-center ">
-        <svg
-          className="z-50 animate-spin"
-          fill="none"
-          height="48"
-          viewBox="0 0 48 48"
-          width="48"
-          xmlns="http://www.w3.org/2000/svg"
+      <ModalOverlay />
+        <ModalContent
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          background="none"
+          boxShadow="none"
         >
-          <path
-            d="M4 24C4 35.0457 12.9543 44 24 44C35.0457 44 44 35.0457 44 24C44 12.9543 35.0457 4 24 4"
-            stroke="##0f172a"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="4"
-          />
-        </svg>
-      </div>
-    </ModalContent>
-  </Modal>
+          <div className="w-screen h-screen backdrop-blur-sm z-50 flex justify-center items-center ">
+            <svg
+              className="z-50 animate-spin"
+              fill="none"
+              height="48"
+              viewBox="0 0 48 48"
+              width="48"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 24C4 35.0457 12.9543 44 24 44C35.0457 44 44 35.0457 44 24C44 12.9543 35.0457 4 24 4"
+                stroke="##0f172a"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="4"
+              />
+            </svg>
+          </div>
+        </ModalContent>
+    </Modal>
   )
 }
 
--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -5,7 +5,12 @@ const Textarea = ({title,name,id,value,onChange,onBlur,errorMessage,disabled}) =
   return (
     <div className='w-full flex flex-col justify-between items-center gap-3 lg:w-[680px] lg:gap-3'>
         <h2 className='font-medium'>{title}</h2>
-        <textarea name={name} id={id} cols="45" rows="5" className='border w-52 p-2 text-center outline-none lg:w-5/6 rounded-lg text-black'
+        <textarea 
+        cols="45" 
+        rows="5" 
+        className='border w-52 p-2 text-center outline-none lg:w-5/6 rounded-lg text-black'
+        name={name} 
+        id={id} 
         disabled={disabled}
         value={value}
         onChange={onChange} 
