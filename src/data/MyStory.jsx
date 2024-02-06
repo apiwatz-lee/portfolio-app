887c9710e27448fff0b9fdee141ac406bbaf515c
Apiwatz Lee <apiwatz.lee@gmail.com>
2024-02-06 23:50:36 +0700

    feat:add starting new career path story

--- src/data/MyStory.jsx
+++ src/data/MyStory.jsx
@@ -1,107 +1,117 @@
-import child from "../assets/story/child1.jpg"
-import graduated from "../assets/story/graduated.jpg"
-import firstJob from "../assets/story/firstJob.jpg"
-import guider from "../assets/story/guider.jpg"
-import learning from "../assets/story/learning.jpg"
-import firstResign from "../assets/story/experience.jpg"
-import bootcamp from "../assets/story/bootcamp.jpg"
-import primary from "../assets/story/primarySchool.jpg"
-import achievement from "../assets/story/achievement.jpg"
-import techupCer from "../assets/story/techupCer.jpg"
-
+import child from '../assets/story/child1.jpg';
+import graduated from '../assets/story/graduated.jpg';
+import firstJob from '../assets/story/firstJob.jpg';
+import guider from '../assets/story/guider.jpg';
+import learning from '../assets/story/learning.jpg';
+import firstResign from '../assets/story/experience.jpg';
+import bootcamp from '../assets/story/bootcamp.jpg';
+import primary from '../assets/story/primarySchool.jpg';
+import achievement from '../assets/story/achievement.jpg';
+import techupCer from '../assets/story/techupCer.jpg';
+import firstJobAsDev from '../assets/story/firstJobAsDev.jpg';
 
 export const story = [
-    {
-        id:1,
-        title:'1995 - When I was a Child',
-        description:`I am Thai of Chinese descent, born and raised in the warm family atmosphere of Hat Yai, Thailand. 
+  {
+    id: 1,
+    title: '1995 - When I was a Child',
+    description: `I am Thai of Chinese descent, born and raised in the warm family atmosphere of Hat Yai, Thailand. 
         My father has a strong interest in computers, so I had the opportunity to play with computers since I was a child. 
         Our house even has a dedicated computer room.
         Back then, I wasn't sure about my interests, 
         but I knew I enjoyed playing computer games.
         However, being a kid who spent a lot of time with games seemed somewhat excessive.
         `,
-        img:child,
-    },
-    {
-        id:2,
-        title:'2000 - Primary and Secondary School',
-        description:`During primary school, I was sent to participate in a computer competition and even secured the second prize. 
+    img: child,
+  },
+  {
+    id: 2,
+    title: '2000 - Primary and Secondary School',
+    description: `During primary school, I was sent to participate in a computer competition and even secured the second prize. 
         Unfortunately, we didn't have a mobile phone with a camera at that time, 
         so I'll share this text instead of a photo (Can you guess who I am? 😂).`,
-        img:primary,
-    },
-    {
-        id:3,
-        title:` 2015 to 2019 - Bachelor's Degree Graduation`,
-        description:`As time passed, I continued to live life like any other kid, exploring and playing as I went along. 
+    img: primary,
+  },
+  {
+    id: 3,
+    title: ` 2015 to 2019 - Bachelor's Degree Graduation`,
+    description: `As time passed, I continued to live life like any other kid, exploring and playing as I went along. 
         When it was time for university, given the family business at home, 
         I thought pursuing a degree in Business Administration (BBA) made sense. 
         My plan was to graduate and contribute to managing the family business.`,
-        img:graduated,
-    },
-    {
-        id:4,
-        title:'2019 - My First Job',
-        description:`After completing my studies, my parents wanted me to gain external experience before returning to manage the family business. 
+    img: graduated,
+  },
+  {
+    id: 4,
+    title: '2019 - My First Job',
+    description: `After completing my studies, my parents wanted me to gain external experience before returning to manage the family business. 
         Thus, I worked at Southland Rubber Co., Ltd as a Procurement Officer. 
         My responsibilities included visiting suppliers to build good relationships and analyzing markets to adjust the company's raw material procurement strategy. 
         It was okay, but I questioned if it was what I truly enjoyed.`,
-        img:firstJob,
-    },
-    {
-        id:5,
-        title:'2021 to 2023 - Achievement',
-        description:`Our team were assigned the EcoVadis project, 
+    img: firstJob,
+  },
+  {
+    id: 5,
+    title: '2021 to 2023 - Achievement',
+    description: `Our team were assigned the EcoVadis project, 
         where EcoVadis acted as a third party guaranteeing the sustainability of our operations. 
         I had the opportunity to oversee the Economic Aspect, planning from the initiation of in-house projects, ensuring communication within the company, and monitoring operations throughout the year. 
         Ultimately, we reported these projects to EcoVadis, earning us the Silver Medal in both 2022 and 2023 for our sustainable practices.`,
-        img:achievement,
-    },
-    {
-        id:6,
-        title:'2023 - Turning Point',
-        description:`While working, I was fortunate to meet and be in the same group as an older colleague who is a programmer at the company. 
+    img: achievement,
+  },
+  {
+    id: 6,
+    title: '2023 - Turning Point',
+    description: `While working, I was fortunate to meet and be in the same group as an older colleague who is a programmer at the company. 
         He opened up the world of coding to me, and I believe this is a turning point in my life. 
         After getting to know it, I feel like this is exactly what I've been searching for all along.
          (it was like finding the missing piece of the puzzle that is me)`,
-        img:guider,
-    },
-    {
-        id:7,
-        title:'Apr2023 - Learning Process',
-        description:`I enjoy learning and honing my skills. 
+    img: guider,
+  },
+  {
+    id: 7,
+    title: 'Apr2023 - Learning Process',
+    description: `I enjoy learning and honing my skills. 
         I delight in dedicating time to creatively craft the projects I am passionate about. 
         When the work takes shape, I feel that it truly adds vibrant colors to my life.`,
-        img:learning,
+    img: learning,
   },
-    {
-        id:8,
-        title:'Jun2023 - My First Resignation',
-        description:`Once confident that I genuinely love it, 
+  {
+    id: 8,
+    title: 'Jun2023 - My First Resignation',
+    description: `Once confident that I genuinely love it, 
         I made the decision to resign from my job on June 6, 2023. 
         It was the last day at the company, marking the day I had to step out of my comfort zone to pursue my dreams and change my career path.`,
-        img:firstResign,
-    },
-    {
-        id:9,
-        title:'Jun2023 - Joined Coding Bootcamp',
-        description:`I've decided to enroll in the Full-Stack Developer Bootcamp at TechUp Institute to change my career path. 
+    img: firstResign,
+  },
+  {
+    id: 9,
+    title: 'Jun2023 - Joined Coding Bootcamp',
+    description: `I've decided to enroll in the Full-Stack Developer Bootcamp at TechUp Institute to change my career path. 
         I've flown from Hat Yai to Bangkok, solely focusing on my studies. 
         The course covered HTML, CSS, JAVASCRIPT, REACT.JS, NODE.JS, EXPRESS, POSTGRESQL, MONGODB, 
         GIT AND GITHUB, and I worked on real projects.`,
-        img:bootcamp,
-    },
-    {
-        id:10,
-        title:'Oct2023 - TechUp Graduation',
-        description:`I successfully completed my studies at TechUp Coding Bootcamp. 
+    img: bootcamp,
+  },
+  {
+    id: 10,
+    title: 'Oct2023 - TechUp Graduation',
+    description: `I successfully completed my studies at TechUp Coding Bootcamp. 
         However, the learning journey doesn't end. 
         I'm considering continuing my education with TypeScript and working on a website project for my older brother related to real estate. 
         While I am in the process of job hunting, 
         ultimately, I have no idea what the future holds – whether I will secure a job or not. What I do know is that currently, 
         I love what I am doing, and I believe that my work can help solve problems or create value for those around me. 
         I am confident that what I do can contribute positively to the people in my life`,
-        img:techupCer,
-    },
-  ]
+    img: techupCer,
+  },
+  {
+    id: 11,
+    title: 'Jan2024 - Starting new career path',
+    description: `After completing the bootcamp, I dedicated two months to honing my coding skills. Subsequently, 
+    I began applying for jobs, and I am delighted to inform everyone that I have started a new job as a full-stack developer 
+    at xSurface Company Limited. It marks a promising beginning for me, 
+    and I am committed to continuous self-improvement while contributing value to the organization every day
+    `,
+    img: firstJobAsDev,
+  },
+];
