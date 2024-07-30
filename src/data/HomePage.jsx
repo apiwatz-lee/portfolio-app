153eebe9d791ee8cd593bb7cf4e55585bca30e75
Apiwat Lee <apiwatz.lee@gmail.com>
2024-07-30 16:36:31 +0700

    edit:update skill

--- src/data/HomePage.jsx
+++ src/data/HomePage.jsx
@@ -1,30 +1,51 @@
-import html from "../assets/logo/html.png"
-import css from "../assets/logo/css.png"
-import js from "../assets/logo/js.png"
-import react from "../assets/logo/react.png"
-import typescript from "../assets/logo/typescript.png"
-import tailwind from "../assets/logo/tailwind.png"
-import node from "../assets/logo/nodejs.png"
-import express from "../assets/logo/express.svg"
-import postgreSQL from "../assets/logo/postgresql.png"
-import monogodb from "../assets/logo/mongodb.svg"
-import vite from "../assets/logo/vite.svg"
+import html from "../assets/logo/html.png";
+import css from "../assets/logo/css.png";
+import js from "../assets/logo/js.png";
+import react from "../assets/logo/react.png";
+import typescript from "../assets/logo/typescript.png";
+import tailwind from "../assets/logo/tailwind.png";
+import node from "../assets/logo/nodejs.png";
+import express from "../assets/logo/express.svg";
+import postgreSQL from "../assets/logo/postgresql.png";
+import monogodb from "../assets/logo/mongodb.svg";
+import vite from "../assets/logo/vite.svg";
+import nextjs from "../assets/logo/nextjs.png";
 
 export const motto = {
-    first:'A man who never stops Learning',
-    second:'A man who never stops Praticing',
-    third:'A man who never stops Focusing',
-  }
+  first: "A man who never stops Learning",
+  second: "A man who never stops Praticing",
+  third: "A man who never stops Focusing",
+};
 
 export const logo = [
-{id:1,name:'html',img:html,url:'https://developer.mozilla.org/en-US/docs/Web/HTML'},
-{id:2,name:'css',img:css,url:'https://developer.mozilla.org/en-US/docs/Web/CSS'},
-{id:3,name:'js',img:js,url:'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
-{id:4,name:'react',img:react,url:'https://react.dev/'},
-{id:5,name:'vite',img:vite,url:'https://vitejs.dev/'},
-{id:6,name:'tailwind',img:tailwind,url:'https://tailwindcss.com/'},
-{id:7,name:'node',img:node,url:'https://nodejs.org/en/docs'},
-{id:8,name:'express',img:express,url:'https://expressjs.com/'},
-{id:9,name:'postgreSQL',img:postgreSQL,url:'https://www.postgresql.org/'},
-{id:10,name:'mongoDB',img:monogodb,url:'https://www.mongodb.com/'},
-]
+  {
+    id: 1,
+    name: "html",
+    img: html,
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    name: "css",
+    img: css,
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "js",
+    img: js,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  { id: 4, name: "react", img: react, url: "https://react.dev/" },
+  { id: 5, name: "nextjs", img: nextjs, url: "https://nextjs.org/" },
+  { id: 6, name: "tailwind", img: tailwind, url: "https://tailwindcss.com/" },
+  { id: 7, name: "node", img: node, url: "https://nodejs.org/en/docs" },
+  { id: 8, name: "express", img: express, url: "https://expressjs.com/" },
+  {
+    id: 9,
+    name: "postgreSQL",
+    img: postgreSQL,
+    url: "https://www.postgresql.org/",
+  },
+  { id: 10, name: "mongoDB", img: monogodb, url: "https://www.mongodb.com/" },
+];
